4b4b9536bb14db0faba4a5a9e8f0da381e356229
Robert Justin Monzingo <robertjmonzingo@gmail.com>
2022-02-04 06:26:56 -0600

    removed colsole logs and unecessary things

--- neighborhood/src/components/RoomWall.tsx
+++ neighborhood/src/components/RoomWall.tsx
@@ -1,15 +1,10 @@
 /* eslint-disable react/require-render-return */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import * as web3 from '@solana/web3.js';
-import { Button } from 'react-bootstrap';
 import * as metadata from "@metaplex-foundation/mpl-token-metadata";
 import PictureComponent from './Picture';
-import { Outlet } from 'react-router-dom';
 
 const axios = require('axios').default;
-//TODO: wrap this in a wallet adapter for nice UI sign in and then create a metadata object
-let nftsmetadata: any[];
-let nftImages: string[] = [];
 var connection = new web3.Connection(
     web3.clusterApiUrl('mainnet-beta'),
     'confirmed',
@@ -29,8 +24,6 @@ class RoomWallComponent extends React.Component<any,any> {
                 console.log(resp)
                 return await this.getImageForEachString(resp)
         });
-
-        console.log('This happens 4th.');
         return promise;
     }
     
@@ -41,20 +34,13 @@ class RoomWallComponent extends React.Component<any,any> {
         })
         
         const bar = await Promise.all(promiseList);
-        console.log(bar);
-        console.log('This happens 6th.');
         return bar;
     }
 
     componentDidMount() {
-
-        console.log('This happens 3rd.');
-    
         this.setState({ loading: 'true' });
         this.loadData()
         .then((data) => {
-            console.log(data)
-          console.log('This happens 7th.');
           this.setState({
             data: data,
             loading: 'false'
@@ -64,17 +50,13 @@ class RoomWallComponent extends React.Component<any,any> {
 
       render() {
         if (this.state.loading === 'initial') {
-            console.log('This happens 2nd - after the class is constructed. You will not see this element because React is still computing changes to the DOM.');
             return <h2>Intializing...</h2>;
         }
 
         if (this.state.loading === 'true') {
-            console.log('This happens 5th - when waiting for data.');
             return <h2>Loading...</h2>;
           }
           
-        console.log('This happens 8th - after I get data.');
-        console.log(this.state.data);
         return (
           <div>
                 {this.state.data.map((item: { data: { image: React.Key | null | undefined; }; }) => {
