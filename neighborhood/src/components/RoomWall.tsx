09dfecc7e6b55464e846cedf4bb8e703f9820c98
Robert Justin Monzingo <robertjmonzingo@gmail.com>
2022-02-10 18:36:06 -0600

    changes for final room

--- neighborhood/src/components/RoomWall.tsx
+++ neighborhood/src/components/RoomWall.tsx
@@ -1,57 +1,11 @@
 /* eslint-disable react/require-render-return */
 import React, { FC, useCallback } from 'react';
-import * as web3 from '@solana/web3.js';
-import * as metadata from "@metaplex-foundation/mpl-token-metadata";
-import PictureComponent from './Picture';
 import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { useNavigate, useParams  } from 'react-router-dom';
 
-const axios = require('axios').default;
-var connection = new web3.Connection(
-    web3.clusterApiUrl('mainnet-beta'),
-    'confirmed',
-  );
 class RoomWallComponent extends React.Component<any,any> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-          loading: true,
-          data: []
-        };
-    }
-    loadData() {
-        //var isHomeOwner = metadata.Metadata.getInfo()
-        var promise = metadata.Metadata.findDataByOwner(connection, "ckFTtPQAVcHsWmdRKX5i49mUyVCRGTjLQ3VpKzgJk4Z").then(async (resp) =>{
-                console.log(resp)
-                return await this.getImageForEachString(resp)
-        });
-        return promise;
-    }
     
-    async getImageForEachString(resp: any[]) {
-        let promiseList: any[] = [];
-        resp.forEach(async nft => {
-            promiseList.push(axios.get(nft.data.uri));
-        })
-        
-        const bar = await Promise.all(promiseList);
-        return bar;
-    }
-
-    componentDidMount() {
-      debugger;
-        this.setState({ loading: true });
-        this.loadData()
-        .then((data) => {
-          this.setState({
-            data: data,
-            loading: false
-          });
-          console.log(this.state)
-        });
-      }  
-
       render() {
         if (this.state.loading === true) {
           return ( <>
@@ -70,16 +24,6 @@ class RoomWallComponent extends React.Component<any,any> {
            
           </>);
         }
-        
-        if(this.state.loading === false){
-          return ( <>
-            <div>
-                {this.state.data.map((item: { data: { image: React.Key | null | undefined; }; }) => {
-                    return <PictureComponent key={item.data.image} nft={item.data.image} />
-                })}
-            </div>
-          </>);
-        }
       }
 }
 
@@ -89,6 +33,12 @@ export const SendOneLamportToRandomAddress: FC = props => {
   let navigate = useNavigate();
   const { publicKey } = useWallet();
   const isHouseOwner: boolean = true;
+
+  const getShortWalletString = () => {
+    return publicKey?.toString().substring(0, 4) + '...' + publicKey?.toString().slice(publicKey?.toString().length - 4);
+  }
+  const shortWallet = publicKey?.toLocaleString() !== null ? getShortWalletString() : '';
+
   const onClick = useCallback(() => {
       if(publicKey?.toLocaleString() !== undefined && isHouseOwner){
         //logic says house owner so redirect to room
@@ -102,15 +52,15 @@ export const SendOneLamportToRandomAddress: FC = props => {
   }
   return (
     <>
-      <h2 style={{color: 'white', paddingLeft: '20px', paddingTop:'10px', fontFamily: 'cursive', cursor: 'pointer'}}>House # {number}</h2>
-      <h2 style={{color: 'white', paddingLeft: '20px', paddingTop:'10px', fontFamily: 'cursive', cursor: 'pointer'}}>Owner {publicKey?.toLocaleString()}</h2>
-      <h2 onClick={onClick} style={{color: 'white', paddingRight: '20px', paddingTop:'10px', fontFamily: 'cursive', cursor: 'pointer', float: 'right'}}>Login</h2>
-      <div>
+      <div style={{paddingRight: '30px', paddingTop:'20px', float: 'right'}}>
         <WalletMultiButton />
       </div>
-     <div>
+      <div style={{paddingRight: '30px', paddingTop:'20px', float: 'right', visibility: publicKey === undefined || publicKey?.toLocaleString() === '' || publicKey === null ? 'hidden' : 'visible' }}>
        <WalletDisconnectButton  />
      </div>
+      <h2 style={{color: 'white', paddingLeft: '20px', paddingTop:'10px', fontFamily: 'cursive', cursor: 'pointer'}}>House # {number}</h2>
+      <h2 style={{color: 'white', paddingLeft: '20px', paddingTop:'10px', fontFamily: 'cursive', cursor: 'pointer'}}>Owner {shortWallet}</h2>
+      <h2 onClick={onClick} style={{color: 'white', paddingRight: '20px', fontFamily: 'cursive', cursor: 'pointer', float: 'right', visibility: publicKey === undefined || publicKey?.toString() === '' || publicKey === null ? 'hidden' : 'visible'}}>Chose Image</h2>  
     </>
   );
 };
--- neighborhood/src/pages/NFTSelectionPage.tsx
+++ neighborhood/src/pages/NFTSelectionPage.tsx
@@ -1,13 +1,97 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import * as web3 from '@solana/web3.js';
+import * as metadata from "@metaplex-foundation/mpl-token-metadata";
+import PictureComponent from '../components/Picture';
 
-export interface INFTCollectionPageProps {}
+const axios = require('axios').default;
+var connection = new web3.Connection(
+    web3.clusterApiUrl('mainnet-beta'),
+    'confirmed',
+);
 
-const NFTCollectionPage: React.FunctionComponent<INFTCollectionPageProps> = (props) => {
+const welcomeHeader = 'WELCOME HOME DADDY!' + ' <3';
+
+const NFTCollectionPage: React.FunctionComponent = (props) => {
+    let { pubKey } = useParams();
     return (
-        <div style={{ backgroundColor: '#B1C3F5', backgroundSize: '100% 100%', height:'100%' }}>
-
+        <div style={{ backgroundColor: '#B1C3F5', backgroundSize: '100% 100%', height: '100%', textAlign: 'center' }}>
+            <div>
+                <h1 style={{ color: 'white', paddingTop: '40px', fontFamily: 'cursive' }}>{welcomeHeader}</h1>
+            </div>
+            <div>
+                <h1 style={{ color: 'white', paddingTop: '40px', fontFamily: 'cursive' }}>SELECT WHOME TO PUT ON DISPLAY</h1>
+            </div>
+            <RenderNFTs pubKey={pubKey}/>
         </div>
-    );
+    )
 };
+
+class RenderNFTs extends React.Component<any,any> {
+    constructor(props: any) {
+        super(props);
+        console.log(props);
+        this.state = {
+          loading: 'initial',
+          data: [],
+          pubKey: props.pubKey
+        };
+    }
+
+    loadData() {
+        var promise = metadata.Metadata.findDataByOwner(connection, this.state.pubKey.toLocaleString()).then(async (resp) =>{
+                console.log(resp)
+                return await this.getImageForEachString(resp)
+        });
+        return promise;
+    }
+    
+    async getImageForEachString(resp: any[]) {
+        let promiseList: any[] = [];
+        resp.forEach(async nft => {
+            promiseList.push(axios.get(nft.data.uri));
+        })
+        
+        const bar = await Promise.all(promiseList);
+        return bar;
+    }
+
+    componentDidMount() {
+      debugger;
+        this.setState({ loading: 'true' });
+        this.loadData()
+        .then((data) => {
+          this.setState({
+            data: data,
+            loading: 'false'
+          });
+          console.log(this.state)
+        });
+      }  
+
+      render() {
+        if (this.state.loading === 'initial') {
+            return ( <>
+              <h2>INITIALIZING</h2>
+            </>);
+          }
+  
+          if (this.state.loading === 'true') {
+            return ( <>
+                <h2>LOADING NFTs</h2>
+              </>);
+          }
+          
+          if(this.state.loading === 'true'){
+            return ( <>
+              <div>
+                  {this.state.data.map((item: { data: { image: React.Key | null | undefined; }; }) => {
+                      return <PictureComponent key={item.data.image} nft={item.data.image} />
+                  })}
+              </div>
+            </>);
+      }
+}
+}
 
 export default NFTCollectionPage;
