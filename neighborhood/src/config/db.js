7b6a548a7cbf0b96fb9a744b57ae9ff21496c41f
Robert Justin Monzingo <robertjmonzingo@gmail.com>
2022-02-12 01:35:41 -0600

    connection update

--- neighborhood/src/config/db.js
+++ neighborhood/src/config/db.js
@@ -1,6 +1,6 @@
 import faunadb from 'faunadb';
 const client = new faunadb.Client({
-  secret: process.env.DB_KEY
+  secret: process.env.config.DB_KEY
 });
 const q = faunadb.query;
 export { client, q };
